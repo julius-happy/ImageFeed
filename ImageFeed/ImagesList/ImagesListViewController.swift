7501227697cc7aa4310428a5c07c9605dfae3e9d
Julius <juliushappy@icloud.com>
2023-05-24 00:47:25 +0300

    sptint_8.

--- ImageFeed/ImagesList/ImagesListViewController.swift
+++ ImageFeed/ImagesList/ImagesListViewController.swift
@@ -1,6 +1,11 @@
 
 import UIKit
 
+    //Do statusBar white
+    var preferredStatusBarStyle: UIStatusBarStyle {
+       return .lightContent
+    }
+
 class ImagesListViewController: UIViewController {
     @IBOutlet private var tableView: UITableView!
     
